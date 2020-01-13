7fd21031888fd3e3ebba6653d0ba57185bc45c87
jlpobrien <56231702+jlpobrien@users.noreply.github.com>
2020-01-13 22:31:27 +0000

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,18 @@
+setwd("")
+
+fileurl = "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+if (!file.exists('./Electric power consumption.zip')){
+  download.file(fileurl,"./Electric power consumption.zip")
+  unzip("Electric power consumption.zip", exdir = getwd())
+}
+
+#plot1
+data <- read.table("./household_power_consumption.txt", header=T, sep=";", na.strings = "?")
+
+data[,"Date"] <- as.Date(data[,"Date"],format = "%d/%m/%Y")
+edited_data<-subset(data,Date == "2007-02-01" | Date == "2007-02-02")
+
+hist(edited_data$Global_active_power, main = "Global Active Power",  xlab= "Global Active Power (kilowatts)", ylab = "Frequency", col= "Red")
+
+dev.copy(png, file = "plot1.png", height=480, width=480)
+dev.off()
